4e8dc0520b3e1682ba471e3a7b9633891df10998
Ian Sandes <ian.sandes@instruct.com.br>
2021-09-02 22:28:24 -0300

    feat: creates route to sync agents to db

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import requests
 
-from os import name
 from typing import List
 from fastapi import Depends, FastAPI
 
-import schemas
+
+from db import models, repository, schemas
+from db.database import SessionLocal, engine
+
+from sqlalchemy.orm import Session
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/agents/", response_model=List[schemas.Agent])
-def get_agents():
-    agents = []
+def get_agents(db: Session = Depends(get_db)):
+    agents = repository.get_agents(db)
+    return agents
+
+
+@app.get("/sync_agents/")
+def sync_agents(db: Session = Depends(get_db)):
     agents_return = requests.get("https://valorant-api.com/v1/agents/?language=pt-BR")
     agents_return = agents_return.json()
     for agent in agents_return["data"]:
@@ -29,5 +48,9 @@ def get_agents():
                 image=agent["displayIcon"],
                 role=agent["role"]["displayName"],
             )
-        agents.append(new_agent)
-    return agents
+        db_agent = repository.get_agent(db, new_agent.name)
+        if db_agent:
+            continue
+        repository.create_agent(db=db, agent=new_agent)
+
+    return {"message": "Agents was sync!"}
