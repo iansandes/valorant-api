ebc11e885543a8e0feee301fca3d0a9563f84468
Ian Sandes <ian.sandes@instruct.com.br>
2021-09-13 17:12:56 -0300

    fix: change url to get agents

--- main.py
+++ main.py
@@ -42,24 +42,16 @@ def get_agents(db: Session = Depends(get_db)):
 @app.get("/sync_agents/")
 def sync_agents(db: Session = Depends(get_db)):
     agents_return = requests.get(
-        "https://valorant-api.com/v1/agents/?language=pt-BR"
-    )  # noqa
+        "https://valorant-api.com/v1/agents?isPlayableCharacter=true&language=pt-BR"  # noqa
+    )
     agents_return = agents_return.json()
     for agent in agents_return["data"]:
-        if agent["displayName"] == "Sova":
-            new_agent = schemas.Agent(
-                name=agent["displayName"],
-                description=agent["description"],
-                image="https://vignette.wikia.nocookie.net/valorant/images/6/61/Sova_artwork.png/revision/latest?cb=20200602020314",  # noqa
-                role="Iniciador",
-            )
-        else:
-            new_agent = schemas.Agent(
-                name=agent["displayName"],
-                description=agent["description"],
-                image=agent["fullPortrait"],
-                role=agent["role"]["displayName"],
-            )
+        new_agent = schemas.Agent(
+            name=agent["displayName"],
+            description=agent["description"],
+            image=agent["fullPortrait"],
+            role=agent["role"]["displayName"],
+        )
         db_agent = repository.get_agent(db, new_agent.name)
         if db_agent:
             continue
